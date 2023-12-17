38381cb92e3a15a1abc6452d5c826a12a8540a1d
Aboubkr <aboubkrmohamed86@gmail.com>
2023-12-17 20:22:21 +0200

    second push

--- js/tlb.js
+++ js/tlb.js
@@ -1,8 +1,28 @@
 $('.autoplay').slick({
     slidesToShow: 5,
     slidesToScroll: 3,
-    autoplay: false,
+    autoplay: true,
     autoplaySpeed: 2000,
   })
 
+  
+
+  // loading
+
+  const loading =document.getElementById('loading');
+  const section =document.getElementById('MainPage');
+  
+  function myFunction() {
+  setTimeout(()=>{
+  document.getElementById("loading").style.display = "none";
+
+  document.getElementById("MainPage").style.display = "block";
+
+  
+   
+  }, 3000);
+  }
+  
+  myFunction()
+  
   
